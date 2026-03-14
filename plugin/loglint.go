12bba352f8d32a583f1ff68ec5346a895b67281c
nessornot <nessornot404@gmail.com>
2026-03-14 23:13:55 +0300

    feat: implement part 4

--- plugin/loglint.go
+++ plugin/loglint.go
@@ -2,16 +2,27 @@ package plugin
 
 import (
 	"golang.org/x/tools/go/analysis"
-
+	"github.com/golangci/plugin-module-register/register"
 	"github.com/nessornot/loglint/internal/analyzer"
 )
 
-// golangci-lint entry point
-func New(conf any) ([]*analysis.Analyzer, error) {
-	// TODO: handle custom regexp
-	
-	analyzers := make([] *analysis.Analyzer, 1)
+func init() {
+	register.Plugin("loglint", New)
+}
+
+func New(conf any) (register.LinterPlugin, error) {
+	pluginInstance := &loglintPlugin{}
+	return pluginInstance, nil
+}
+
+type loglintPlugin struct{}
+
+func (p *loglintPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	analyzers := make([]*analysis.Analyzer, 1)
 	analyzers[0] = analyzer.Analyzer
-	
 	return analyzers, nil
+}
+
+func (p *loglintPlugin) GetLoadMode() string {
+	return register.LoadModeTypesInfo
 }
